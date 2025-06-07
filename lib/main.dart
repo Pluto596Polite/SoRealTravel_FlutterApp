9135dac7b83714b9aaad60a61fed3e0c1a3a65ec
Pluto596Polite <avanwyk191@gmail.com>
2025-06-07 18:39:55 +0200

    made a basic flutter program

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sorealtravel_flutter/login_page.dart';
+
 
 void main() => runApp(const MyApp());
 
@@ -9,36 +9,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Root widget
       home: Scaffold(
         appBar: AppBar(title: const Text('My Home Page')),
         body: Center(
-          child: Builder(
-            builder: (context) {
-              //the following is used to organise the main widgets in a column from top to bottom
-              return Column(
-                children: [
-                  const Text('Hello, World!'),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      print('Click!');
-                    },
-                    child: const Text('Please click me!'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Navigate to the second page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginPage()),
-                      );
-                    },
-                    child: const Text('Go to Login Page'),
-                  ),
-                ],
-              );
-            },
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('Hello, World!'),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  print('Click!');
+                },
+                child: const Text('Please click me!'),
+              ),
+              const SizedBox(height: 20),
+            ],
           ),
         ),
       ),
